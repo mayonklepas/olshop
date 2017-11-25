0a5655060ced7b4d5ee85b91734fdb000d899fda
mayonklepas <imonks21@gmail.com>
2017-11-25 19:12:34 +0800

    versi ke 2

---
+++ app/src/main/java/com/mizan/emha/Detailbarang2Activity.java
@@ -0,0 +1,13 @@
+package com.mizan.emha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detailbarang2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailbarang2);
+    }
+}
